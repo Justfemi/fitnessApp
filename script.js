c31d75a0778453dd2c65496969ef60b3732f5158
Justfemi <ajibadeolufemi94@gmail.com>
2022-05-07 17:44:30 +0100

    ch:updated sections, styles and script

--- script.js
+++ script.js
@@ -11,22 +11,52 @@ window.addEventListener('scroll', () => {
   navbar.classList.remove('active');
 });
 
-let swiper = new Swiper ('.home-slider', {
+// let swiper = new Swiper ('.home-slider', {
+//   spaceBetween: 20,
+//   effect: "fade",
+//   loop: true,
+//   grabCursor: true,
+//   centeredSlides: true,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+
+// let swiper = new Swiper ('.review-slider', {
+//   spaceBetween: 20,
+//   loop: true,
+//   grabCursor: true,
+//   autoplay: {
+//     delay: 7500,
+//     disableOnInteraction: false,
+//   },
+//   breakpoints:{
+//     0:{
+//       sliderPerView:1,
+//     },
+//     600:{
+//       sliderPerView:2,
+//     }
+//   }
+// });
+
+let swiperx = new Swiper ('.blogs-slider', {
   spaceBetween: 20,
-  effect: "fade",
   loop: true,
   grabCursor: true,
-  centeredSlides: true,
-  pagination: {
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+    pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-});
-
-let swiper2 = new Swiper ('.review-slider', {
-  spaceBetween: 20,
-  loop: true,
-  grabCursor: true,
   breakpoints:{
     0:{
       sliderPerView:1,
